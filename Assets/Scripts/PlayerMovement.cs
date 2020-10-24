70450b348088d085f81af58a838497659e5c7051
nikofon <nikofon007@gmail.com>
2020-10-24 13:38:59 +0300

    initial

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 move;
+    Rigidbody2D player;
+    public float movespeed;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        player.MovePosition(player.position + movespeed * move);
+    }
+}
