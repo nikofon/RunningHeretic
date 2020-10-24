cffb379288dc7a7fa0e658247d63f493bad0896e
nikofon <nikofon007@gmail.com>
2020-10-25 00:31:39 +0300

    postprocessing

--- Assets/Scripts/TorchMovementAndColout.cs
+++ Assets/Scripts/TorchMovementAndColout.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class TorchMovementAndColout : MonoBehaviour
 {
     private float prevFrequency;
+    public FilmGrain filmGrain;
     private Vector3 offset;
+    public Volume volume;
+    private Collider2D col;
     public Material myMaterial;
     private float startingDistance;
     // Start is called before the first frame update
@@ -13,7 +18,9 @@ public class TorchMovementAndColout : MonoBehaviour
     {
         offset = new Vector3(0.5f, 0.8f, -0.1f);
         startingDistance = Target.instance.ReturnDistance();
-        
+        volume = GameObject.FindObjectOfType<Volume>();
+        volume.profile.TryGet<FilmGrain>(out filmGrain);
+        //filmGrain.intensity.value = 1f;
     }
     private void Update()
     {
@@ -30,11 +37,38 @@ public class TorchMovementAndColout : MonoBehaviour
     {
         if (Mathf.Abs(prevFrequency - (startingDistance - Target.instance.ReturnDistance()) / startingDistance * 20) > 1)
         {
-            Debug.Log((startingDistance - Target.instance.ReturnDistance()) / startingDistance * 20);
             myMaterial.SetFloat("_Multiplier", Mathf.Clamp((startingDistance - Target.instance.ReturnDistance()) / startingDistance * 20, 1f, 20));
             prevFrequency = (startingDistance - Target.instance.ReturnDistance()) / startingDistance * 20;
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Enemy")
+        {
+            StopCoroutine("ChangeIntensityWithTime");
+            Debug.Log("colided");
+            filmGrain.intensity.value = 1f;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(ChangeIntensityWithTime(1f, 0, 0.6f));
+        }
+    }
+    private IEnumerator ChangeIntensityWithTime(float from, float to, float time)
+    {
+        float delta = (from - to) / 50;
+        for(int i = 0; i < 50; i++)
+        {
+            filmGrain.intensity.value -= delta;
+            yield return new WaitForSeconds(time / 50);
+        }
+        yield break;
+    }
+
 
 
 }
